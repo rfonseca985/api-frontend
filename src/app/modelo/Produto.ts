64898c2e103a9901d7de08c977f767e034b86bcc
rfonseca985 <rfonseca985@gmail.com>
2024-03-10 19:36:09 -0300

    Frontend pronto para o deploy

---
+++ src/app/modelo/Produto.ts
@@ -0,0 +1,17 @@
+// A classe Produto representa um produto na aplicação.
+export class Produto{
+  // O identificador único do produto.
+  id:number = 0;
+
+  // O nome do produto.
+  nome:string='';
+
+  // A quantidade do produto em estoque.
+  quantidade:number= 0;
+
+  // O valor do produto.
+  valor:number = 0;
+
+  // Observações adicionais sobre o produto.
+  observacao:string = '';
+}
